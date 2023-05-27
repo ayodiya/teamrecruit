80c8eed8aa30fa1708cec00475dfa3fbdf8ca9a2
ayodiya <devayodiya@gmail.com>
2023-05-27 16:33:38 +0100

    add material ui

--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
+import CssBaseline from '@mui/material/CssBaseline'
+import { ThemeProvider } from '@mui/material/styles'
+
+import { theme } from './utils/theme'
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <CssBaseline />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 )
 
---
+++ src/utils/theme.js
@@ -0,0 +1,17 @@
+import { createTheme } from '@mui/material/styles'
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#375CF5'
+    },
+    secondary: {
+      main: '#F69220',
+      subMain: '#EA088B'
+    },
+    textColor: {
+      main: '#202A50',
+      subMain: '#AFB3C0'
+    }
+  }
+})
